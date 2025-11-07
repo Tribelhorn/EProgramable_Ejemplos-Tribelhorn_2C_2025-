da84b497e81d7d1badcea457090d653be4c8a8c6
cristian <cris.tribelhor@gmail.com>
2025-11-07 15:11:29 -0300

    Pastillero andando

--- firmware/examples/proyecto_final_bis/main/proyecto_final_bis.c
+++ firmware/examples/proyecto_final_bis/main/proyecto_final_bis.c
@@ -41,7 +41,7 @@ uint16_t posicion = 0;
 
 volatile uint16_t pasos_motor = 1200;
 
-uint32_t PERIODO = 30000000;
+uint32_t PERIODO = 40000000;
 
 TaskHandle_t preguntar_task_handle = NULL;	
 TaskHandle_t mover_task_handle = NULL;
@@ -52,7 +52,7 @@ stepper_t motor = {
     .coilC = GPIO_20,
     .coilD = GPIO_19,
     .mode = STEPPER_FULL_STEP,
-    .step_delay_ms = 50  /**< tiempo entre pasos en ms */
+    .step_delay_ms = 15  /**< tiempo entre pasos en ms */
 };
 
 uint16_t casillas[14] ={9999,9999,9999,
@@ -71,11 +71,11 @@ uint16_t casillas[14] ={9999,9999,9999,
 
 
 void PuestaACero(){
-    while(GPIORead(GPIO_13)==1){
+    while(GPIORead(GPIO_12)==1){
         vTaskDelay(10 / portTICK_PERIOD_MS);    
         StepperStep(&motor, 50);
+        LedToggle(LED_2);
     }
-    //StepperStep(&motor, 600);
 }
 
 
@@ -87,8 +87,7 @@ static void tarea_preguntar(void *pvParameter) {
         for (int i = 0; i < 14; i++) {
             if (casillas[i] == hora_militar) {
                 control_motor = !control_motor;
-                //LedToggle(LED_3);
-                posicion = i;
+                posicion = i+1;
             }
             else {
                 PuestaACero();
@@ -98,22 +97,27 @@ static void tarea_preguntar(void *pvParameter) {
 }
 
 static void tarea_mover(void *pvParameter) {
-    if (control_motor) {
-        BuzzerOn();        
-        StepperStep(&motor, posicion*pasos_motor);
-        uint16_t frec = 440;
-        uint16_t tiempo = 3000;
-        BuzzerPlayTone(frec, tiempo);
-        BuzzerOff();
-        control_motor = 0 ;
+    while (1) {    
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+        if (control_motor) {
+            BuzzerOn();        
+            StepperStep(&motor, posicion*pasos_motor);
+            uint16_t frec = 440;
+            uint16_t tiempo = 3000;
+            BuzzerPlayTone(frec, tiempo);
+            BuzzerOff();
+            control_motor = 0;
+            posicion = 0;
+        }
     }
 }
+
 
 void leer_info(uint8_t * data, uint8_t length){
     //Lee el horario de toma en formato (P-casilla-horario militar) = (P-CC-HHMM)/
     //O setea la hora actual como (A-horario militar) = (A-HHMM) //
     if (data [0] == LETRA_A && length >= 6) {
-        uint16_t hora_actual = (data[2]-NUMERO_0)*10 + (data[3]-NUMERO_0); // 48 es el 0 en ASCII
+        uint16_t hora_actual = (data[2]-NUMERO_0)*10 + (data[3]-NUMERO_0); //Se le resta el 0 en ASCII
         uint16_t min_actual = (data[4]-NUMERO_0)*10 + (data[5]-NUMERO_0);
         fecha_hora.hour = hora_actual;
         fecha_hora.min = min_actual;
@@ -137,43 +141,44 @@ void Fun_Timer(void* param){
 void app_main(void){
 
     LedsInit();
-    LedOn(LED_3);
 
-    // RtcConfig(&fecha_hora);
+    RtcConfig(&fecha_hora);
 
-    // ble_config_t ble_configuration = {
-    //     .device_name = "PASTILLERO",
-    //     .func_p = leer_info  //Deberia ir directo a la función o una con vtask//
-    // };
+    ble_config_t ble_configuration = {
+        .device_name = "PASTILLERO",
+        .func_p = leer_info  //Deberia ir directo a la función o una con vtask//
+    };
 
-    // timer_config_t timer_1 = {				//Esta struct la definen en los drivers//
-    //     .timer = TIMER_A,					//Hay 3 timers disponibles para usar
-    //     .period = PERIODO,					//Periodo en microsegundos 
-    //     .func_p = Fun_Timer,				//El nombre de una función es un puntero//
-    //     .param_p = NULL
-    // };
+    timer_config_t timer_1 = {				//Esta struct la definen en los drivers//
+        .timer = TIMER_A,					//Hay 3 timers disponibles para usar
+        .period = PERIODO,					//Periodo en microsegundos 
+        .func_p = Fun_Timer,				//El nombre de una función es un puntero//
+        .param_p = NULL
+    };
 
-	// xTaskCreate(&tarea_preguntar, "Preguntar", 4096, NULL, 5, &preguntar_task_handle);
-    // xTaskCreate(&tarea_mover, "Mover", 4096, NULL, 5, &mover_task_handle);
+
+	xTaskCreate(&tarea_preguntar, "Preguntar", 4096, NULL, 5, &preguntar_task_handle);
+    xTaskCreate(&tarea_mover, "Mover", 4096, NULL, 5, &mover_task_handle);
 
 
     StepperInit(&motor);
+    GPIOInit(GPIO_12, GPIO_INPUT);   //Sensor//
+
     PuestaACero();
-    // GPIOInit(GPIO_13, GPIO_OUTPUT);
 
-    // BuzzerInit(GPIO_13);
+    GPIOInit(GPIO_13, GPIO_OUTPUT);
+
+    BuzzerInit(GPIO_13);
+
+    
 
 
-    // GPIOInit(GPIO_12, GPIO_INPUT);   //Sensor//
+    BleInit(&ble_configuration);
 
-    // BleInit(&ble_configuration);
-
-    // TimerInit(&timer_1);
-
-    //StepperStep(&motor, 5000);
+    TimerInit(&timer_1);
   
     //* Inicialización del conteo de timers */
-    // TimerStart(timer_1.timer);
+    TimerStart(timer_1.timer);
     while(1){
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
         switch(BleStatus()) {
